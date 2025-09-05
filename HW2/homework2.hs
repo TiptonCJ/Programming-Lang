2b9960c6047a2299d62fccd07207ef46d2817732
TiptonCJ <118553253+TiptonCJ@users.noreply.github.com>
2025-09-04 21:56:09 -0400

    hw2 done

--- HW2/homework2.hs
+++ HW2/homework2.hs
@@ -37,32 +37,39 @@ genTails (x:xs) =  (x:xs) : genTails xs
 
 --genSubstrings
 genSubstrings :: String -> [String]
-genSubstrings "" = []
+genSubstrings "" = [""]
 genSubstrings xs = "" : concatMap genTails (genPrefix xs)
 
 --replacePrefix
 replacePrefix :: (String,String) -> String -> String
-replacePrefix (x,[]) zs = zs
 replacePrefix (x,y) [] = []
 replacePrefix (x,y) zs = y ++ drop (length x) zs
 
 replaceString :: (String,String) -> String -> String
 replaceString (xs,ys) "" = ""
 replaceString (xs,ys) (z:zs)
-    | isPrefix xs (z:zs) = replacePrefix (xs,ys) zs
+    | isPrefix xs (z:zs) = replacePrefix (xs,ys) (z:zs)
     | otherwise = z : replaceString (xs,ys) zs
 
 --cypher
 
-lookUp :: Char -> [(Char, Char)] -> Maybe Char
-lookup "" [] = Nothing
+lookUp :: Char -> [(Char, Char)] -> Char
+lookup "" [] = []
 lookUp x  ((ly,ry):rest)
-    | x == ly = Just ry
+    | x == ly = ry
     | otherwise = lookUp x rest
 
+encode :: [(Char,Char)] -> String -> String
+encode table [] = []
+encode table (y:ys) = lookUp y table : encode table ys
+
+makeTable :: String -> String -> [(Char,Char)]
+makeTable xs [] = []
+makeTable [] ys = []
+makeTable xs ys = zip xs ys
+
+caesar :: Int -> [(Char,Char)]
+caesar n = (' ',' ') : makeTable abc (drop n (cycle abc)) where abc = ['A'..'Z']
 
 
-
-
---tester below
 
